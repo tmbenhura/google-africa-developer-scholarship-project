cc83792fa17c55c57e1fcb3114a9695b3a804663
Taurai M. Benhura <tmbenhura@gmail.com>
2020-09-08 22:46:51 +0200

    IMPL: Added Splash Activity

---
+++ app/src/main/java/zw/co/mobility/gads/SplashActivity.kt
@@ -0,0 +1,12 @@
+package zw.co.mobility.gads
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
