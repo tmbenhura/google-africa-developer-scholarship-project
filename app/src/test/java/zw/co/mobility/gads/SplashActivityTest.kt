879461bae3e11b5bfcdec44122dcaa97e57dcfbe
Taurai M. Benhura <tmbenhura@gmail.com>
2020-09-08 23:00:21 +0200

    TEST: Splash Activity Launches Main Activity

--- app/src/test/java/zw/co/mobility/gads/SplashActivityTest.kt
+++ app/src/test/java/zw/co/mobility/gads/SplashActivityTest.kt
@@ -1,10 +1,15 @@
 package zw.co.mobility.gads
 
+import android.content.Intent
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
+
 
 @RunWith(RobolectricTestRunner::class)
 class SplashActivityTest {
@@ -17,5 +22,18 @@ class SplashActivityTest {
             .get()
 
         assertNotNull(activity)
+    }
+
+    @Test
+    fun splash_launchesMainActivity() {
+        val activity = Robolectric.buildActivity<SplashActivity>(SplashActivity::class.java)
+            .create()
+            .start()
+            .resume()
+            .get()
+
+        val expectedIntent = Intent(activity, MainActivity::class.java)
+        val actual: Intent = shadowOf(RuntimeEnvironment.application).getNextStartedActivity()
+        assertEquals(expectedIntent.component, actual.component)
     }
 }
