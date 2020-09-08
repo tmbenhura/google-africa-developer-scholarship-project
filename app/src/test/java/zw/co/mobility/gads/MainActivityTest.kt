ffa81b2a94e028bc0e64867693052af7617349bd
Taurai M. Benhura <tmbenhura@gmail.com>
2020-09-08 23:54:41 +0200

    TEST: Main Activity Can Launch Project Submission

--- app/src/test/java/zw/co/mobility/gads/MainActivityTest.kt
+++ app/src/test/java/zw/co/mobility/gads/MainActivityTest.kt
@@ -1,12 +1,18 @@
 package zw.co.mobility.gads
 
+import android.content.Intent
+import android.view.View
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.annotation.LooperMode
 
-
+@LooperMode(LooperMode.Mode.PAUSED)
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
     @Test
@@ -18,5 +24,16 @@ class MainActivityTest {
             .get()
 
         assertNotNull(activity)
+    }
+
+    @Test
+    fun main_canLaunchProjectSubmissionActivity() {
+        val activity = Robolectric.setupActivity(MainActivity::class.java)
+        val button = activity.findViewById<View>(R.id.submit_project)
+        button.performClick()
+
+        val expectedIntent = Intent(activity, ProjectSubmissionActivity::class.java)
+        val actual = shadowOf(RuntimeEnvironment.application).nextStartedActivity
+        assertEquals(expectedIntent.component, actual.component)
     }
 }
